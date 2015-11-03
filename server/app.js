2595964116ac4ea6fa6863782486df2cbb278711
Jack3113 <jack03113@gmail.com>
2015-11-03 06:53:26 +0100

    feat(projects): Delete project

--- server/app.js
+++ server/app.js
@@ -69,4 +69,17 @@ app.put('/projects/:id', function(req, res) {
   });
 });
 
+app.delete('/projects/:id', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  db.projectModel.remove({
+    _id: req.params.id
+  }, function(err, data) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(JSON.stringify(data));
+    }
+  });
+});
+
 app.listen(8080);
