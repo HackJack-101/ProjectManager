38a0a08d2276ac5dc17d4b46d311d15a99d72d6b
Jack3113 <jack03113@gmail.com>
2015-11-09 17:55:40 +0100

    build(NodeJS app): Update indentation

--- server/app.js
+++ server/app.js
@@ -7,79 +7,79 @@ var bodyParser = require('body-parser');
 var connect = require('connect');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 });
 
-app.get('/', function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.send("Hello World!");
+app.get('/', function (req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.send("Hello World!");
 });
 
-app.get('/projects', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  db.projectModel.find(function(err, data) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(JSON.stringify(data));
-    }
-  });
+app.get('/projects', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	db.projectModel.find(function (err, data) {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.send(JSON.stringify(data));
+		}
+	});
 });
 
-app.get('/projects/:id', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  db.projectModel.findById(req.params.id, function(err, data) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(JSON.stringify(data));
-    }
-  });
+app.get('/projects/:id', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	db.projectModel.findById(req.params.id, function (err, data) {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.send(JSON.stringify(data));
+		}
+	});
 });
 
-app.post('/projects', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  var newProject = new db.projectModel(req.body);
-  newProject.save(function(err) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(req.body);
-    }
-  });
+app.post('/projects', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	var newProject = new db.projectModel(req.body);
+	newProject.save(function (err) {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.send(req.body);
+		}
+	});
 });
 
-app.put('/projects/:id', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  db.projectModel.update({
-    _id: req.params.id
-  }, req.body, function(err) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(JSON.stringify(req.body));
-    }
-  });
+app.put('/projects/:id', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	db.projectModel.update({
+		_id: req.params.id
+	}, req.body, function (err) {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.send(JSON.stringify(req.body));
+		}
+	});
 });
 
-app.delete('/projects/:id', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  db.projectModel.remove({
-    _id: req.params.id
-  }, function(err, data) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(JSON.stringify(data));
-    }
-  });
+app.delete('/projects/:id', function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+	db.projectModel.remove({
+		_id: req.params.id
+	}, function (err, data) {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.send(JSON.stringify(data));
+		}
+	});
 });
 
 app.listen(8080);
