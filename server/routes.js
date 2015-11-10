87175cd3e9409f158c161caddd29f9f59fc2f880
Jack3113 <jack03113@gmail.com>
2015-11-10 04:00:39 +0100

    feat(routes): Update routes for US and tasks

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,8 @@ module.exports = function (app) {
 	'use strict';
 
 	app.use('/projects', require('./api/project'));
+	app.use('/tasks', require('./api/task'));
+	app.use('/userstories', require('./api/userstory'));
 
 	app.route('/', function (req, res) {
 		res.setHeader('Content-Type', 'text/plain');
